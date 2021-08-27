83a5cb6ecae133e08c572f4a34c7b0cdb1afd4d8
Andrew <kolbe1129@gmail.com>
2021-08-27 16:37:15 -0500

    changed signout endpoint to logout

--- auth/src/routes/logout.ts
+++ auth/src/routes/logout.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.post('/api/users/signout', (req, res) => {
+router.post('/api/users/logout', (req, res) => {
     req.session = null;
 
     res.send({})
