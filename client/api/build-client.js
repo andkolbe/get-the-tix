dd96482f0a305ce3ba7f439ee2962ffa94ae5745
Andrew <kolbe1129@gmail.com>
2021-08-28 00:44:54 -0500

    testing client-depl rolling restart

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -8,7 +8,7 @@ const buildClient = ({ req }) => {
       headers: req.headers
     });
   } else {
-    // We must be on the browser
+    // We must be on the browser!
     return axios.create({
       baseUrl: '/'
     });
